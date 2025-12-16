d87ec58ee430f06bac050a30bc867c1116c431bb
Abishek CHaulagain <abishekchaulagain5225@gmail.com>
2025-12-16 21:55:29 +0545

    updated

---
+++ main.asm
@@ -0,0 +1,18 @@
+section .data
+ prompt db "Enter password: "
+ prompt_len equ $ - prompt
+
+ weak_msg db "Weak password", 10
+ weak_len equ $ - weak_msg
+
+ strong_msg db "Strong password", 10
+ strong_msg_len equ $ - strong_msg
+
+section .bss
+      buffer resb 32 ; buffer for password max 32 characters
+
+section .text
+  global _start
+
+_start:
+
